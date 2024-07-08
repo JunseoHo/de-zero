5aceb966a47f27edb5661ab161f7830affa96d76
JunseoHo <junseoho1029@gmail.com>
2024-07-08 10:05:33 +0900

    feat : step6

--- Exp.py
+++ Exp.py
@@ -6,3 +6,8 @@ from Function import Function
 class Exp(Function):
     def forward(self, x):
         return np.exp(x)
+
+    def backward(self, gy):
+        x = self.input.data
+        gx = np.exp(x) * gy
+        return gx
